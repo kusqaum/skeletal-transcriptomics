a3cfb4a6e6c2cabf121bd405cf25e8aebf2653b9
kusqaum <kusqaum1@gmail.com>
2024-06-10 09:53:14 +0100

    fix NMF for loop and plot p-values for NMF

--- 02_exploratoryDataVisualisation.R
+++ 02_exploratoryDataVisualisation.R
@@ -10,7 +10,7 @@ row.names(labels) <- rownames(geneExpressionWithLabels)
 colnames(labels)[1] <- "significant"
 
 #just get the first 50 samples as a subset
-smallGeneExpress <- geneExpressionWithLabels %>% select(1:50)
+#smallGeneExpress <- geneExpressionWithLabels %>% select(1:50)
 smallGeneExpress2 <- geneExpressionWithLabels[,1:50]
 smallGeneExpress2 <- normalize.quantiles(as.matrix(smallGeneExpress2))
 row.names(smallGeneExpress2) <- rownames(geneExpressionWithLabels)
@@ -18,6 +18,7 @@ colnames(smallGeneExpress2) <- colnames(geneExpressionWithLabels[,1:50])
 smallGeneExpress2 <- as.data.frame(smallGeneExpress2)
 
 smallGeneExpress3 <- smallGeneExpress2[apply(smallGeneExpress2!=0,1,all),]
+dim(smallGeneExpress3) #5370 genes left
 
 #log2 transform the data
 smallGeneExpress3 <- log2(smallGeneExpress3)
@@ -61,7 +62,7 @@ noofruns <- 2
 #set seed as well
 res.multiRank <- nmf(abs(smallGeneExpress3[,1:50]), rank = noRanks, nrun=noofruns, seed = 123456)
 #can choose to plo just cophenetic + silhouette
-plot(res.multiRank, 'cophenetic', 'silhouette')
+#plot(res.multiRank, 'cophenetic', 'silhouette')
 
 #look at performance measures of the factorisation
 summary(res.multiRank)
@@ -74,13 +75,13 @@ consensusmap(res.multiRank, labCol = NA, labRow = NA)
 #maxNsCoph <- max(res.multi.method$nsNMF$measures$cophenetic)
 
 #check sumary measures for each method
-compare(res.multi.method)
+#compare(res.multi.method)
 #look at error tracks for each method
-plot(res.multi.method)
-plot(res.multiRank)
+#plot(res.multi.method)
+#plot(res.multiRank)
 
 
-res.multiRank$measures
+res.multiRank$measures$rank
 
 #idxCophenMax
 #res.multiRank$measures$cophenetic
@@ -99,25 +100,36 @@ basis_matrices <- lapply(Wmatrices, function(w){
   data.frame(list(w))
 })
 
-
-nmfPvals <- numeric()
-minPvals <- numeric()
-for (mat in basis_matrices) {
-  #pValue <- rep
-  #View(mat)
-  for (dim in 1:ncol(mat)) {
-    resTtest <- t.test(smallGeneExpress3$significant, mat[, dim])$p.value
-    #mat$pValue <- resTtest
-    nmfPvals <- c(nmfPvals, resTtest)
-    #minPvals <- which.min(nmfPvals)
-    #basis_matrices <- append(basis_matrices[[mat]], resTtest[[mat]])
-    
+nmfMinPvals <- list()
+#go through the length of Ws
+for (mt in 1:length(Wmatrices)) {
+  #get ready list
+  pValues <- numeric()
+  #loop through each col
+  for (dim in 1:ncol(Wmatrices[[mt]])) {
+    pResultNMF <- t.test(smallGeneExpress3$significant, Wmatrices[[mt]][, dim])$p.value
+    #nmfPvals <- c(nmfPvals, pResultNMF)
+    pValues <- c(pValues, pResultNMF)
   }
-  #minPvals <- c(minPvals)
-  
+  #get min p val and their positions and make into list as DFs
+  minP <- min(pValues)
+  minPdim <- which.min(pValues)
+  nmfMinPvals[[mt]] <- data.frame(pVals = minP,
+                              Dimension = ncol(Wmatrices[[mt]]),
+                              Feature=minPdim, 
+                              Algorithm = "NMF")
 }
 
+NMFfeaturesMinP <- do.call("rbind", nmfMinPvals)
+ggplot(NMFfeaturesMinP, aes(x = Dimension, y=-log10(pVals), col = Algorithm)) +
+  geom_point() +
+  stat_smooth() +
+  #theme_bw(base_size = 18) +
+  xlab("k dimensions") + ylab("-log10 P-Value")
 
+# checking a single to-test p-value result here is consistent with table created above.
+tempor <- data.frame(Wmatrices[[5]])
+t.test(smallGeneExpress3$significant, tempor[, 9])$p.value
 
 
 
@@ -125,15 +137,7 @@ for (mat in basis_matrices) {
 
 #perform PCA. each of our data points needs to be a gene so not going to transpose
 pca_res <- prcomp((smallGeneExpress3[,1:50]), scale =T)
-#look at variance
-#summary(pca_res)$importance
-#get 1&2nd PCs to plot
 pcs <- data.frame(pca_res$x)
-#pcs$significant <- smallGeneExpress3$significant
-
-
-#t_subset <- smallGeneExpress[,50:51]
-#t.test(culture_fibroblast ~ significant, data = smallGeneExpress)
 
 tmp <- t.test(smallGeneExpress3$significant, pcs[,"PC1"])
 tmp$p.value
@@ -157,13 +161,15 @@ for (PC in 1:ncol(pcs)) {
   resultfortest <- t.test(smallGeneExpress3$significant,  pcs[,PC])$p.value
   pValsPCA <- c(pValsPCA, resultfortest)
   #resultfortest_pval <- resultfortest$
-  smallestPval <- which.min(pValsPCA)
+  #smallestPval <- which.min(pValsPCA)
 }
-pValsdf <- as.data.frame(pValsPCA); pValsdf$dimension<- colnames(pcs)
-pValsdf$dim <- c(1:ncol(pcs))
+pValsdf <- as.data.frame(pValsPCA); pValsdf$Dimension<- 1:ncol(pcs)
+
+###colnames(pValsdf)[1] <- "pVals"
+pValsdf$Feature <- c(1:ncol(pcs))
 pValsdf$Algorithm <- c('PCA')
 #t.test(smallGeneExpress3$significant, pcs$PC50)$p.value
-ggplot(pValsdf, aes(x=dim, y=-log10(pValsPCA), col = Algorithm)) +
+ggplot(pValsdf, aes(x=Dimension, y=-log10(pValsPCA), col = Algorithm)) +
   geom_point()+
   geom_smooth(method = 'loess') +
   theme_bw(base_size = 18) +
@@ -172,9 +178,9 @@ ggplot(pValsdf, aes(x=dim, y=-log10(pValsPCA), col = Algorithm)) +
 
 #also create a scree plot for pca
 var_explained <- (pca_res$sdev)^2/sum((pca_res$sdev)^2)
-varExpl_df <- data.frame(PC = 1:length(var_explained), 
-                                var_expl = var_explained,
-                         cumulativeVariance = cumsum(var_expl))
+varExpl_df <- data.frame(PC = 1:length(var_explained),
+                         varianceExpl = (var_explained),
+                         cumulativeVariance = cumsum(var_explained))
 
 ggplot(varExpl_df[1:8,], aes(x = PC, y = 100*cumulativeVariance)) +
   geom_bar(stat = 'identity', col = 'black', fill="lightgrey") + 
@@ -186,10 +192,76 @@ ggplot(varExpl_df[1:8,], aes(x = PC, y = 100*cumulativeVariance)) +
   xlab("PC index") + ylab("% explained variance by principal components")
   
   #xlim(0, 10)
+####plot for both algorithms on one page####
+pValsdf2 <- pValsdf
+colnames(pValsdf2)[1]<- "pVals"
+allDimensionReduction <- rbind(NMFfeaturesMinP, pValsdf2)
+ggplot(allDimensionReduction, aes(x = Dimension, y=-log10(pVals), col=Algorithm)) +
+  geom_point() +
+  geom_smooth()+
+  theme_bw(base_size = 20)+
+  xlab("k dimensions") +ylab("-log10 P-Value")+
+  scale_x_continuous(breaks = seq(2,50, 2))
+  
+  
+
 
 ####rough of machine learning:####
-#library(tidymodels)
+library(tidymodels)
+library(themis)
+library(skimr)
+inputForML <- basis_matrices
+#View(Wmatrices[[1]])
 
+inputForML2 <- list()
+for (each in inputForML) {
+#  dataframe <- data.frame(unlist(each))
+  each$significant <- (smallGeneExpress3$significant)
+  inputForML2 <- append(inputForML2, list(each))
+  
+}
+
+str(inputForML2[[2]])
+temp <- inputForML2[[2]]
+#convert outcome to a factor
+temp$significant <- as.factor(temp$significant)
+str(temp)
+skim(temp)
+
+dataSplit <- initial_split(temp, prop = 0.8)
+tempTrain <- training(dataSplit)
+tempTest <- testing(dataSplit)
+
+#look at before sampling
+ggplot(tempTrain, aes(factor(significant)))+
+  geom_bar(aes(y=after_stat(count)/sum(after_stat(count))), colour = "black", fill = "lightgrey")+
+  scale_y_continuous(labels = percent)+
+  xlab("") + ylab("% of genes") +
+  theme_minimal(base_size = 20)
+
+
+temp_rec <- recipe(significant ~ ., data = tempTrain) %>%
+  step_downsample(significant, under_ratio =1)
+temp_rec %>% prep() %>% bake(NULL)
+
+
+
+#how does undersampling look after
+temp_rec %>% 
+  prep()%>% 
+  juice() %>%
+  ggplot(aes(factor(significant))) +
+  geom_bar(aes(y = (after_stat(count))/sum(after_stat(count))), colour="black",fill="lightgrey") +
+  scale_y_continuous(labels = percent) +
+  xlab("") + ylab("% of genes") +
+  theme_minimal(base_size = 20)
+
+library(parsnip)
+show_engines("rand_forest")
+rf_mod <- rand_forest(trees = 100, 
+                      mtry = tune(), 
+                      mode = "classification") %>%
+  set_engine("randomForest")
 
 compressed_PCA <- pcs
 compressed_PCA$significant <- smallGeneExpress3$significant
