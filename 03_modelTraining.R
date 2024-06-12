deda596a673e7b7568a3348e33253d3f8026f1e4
kusqaum <kusqaum1@gmail.com>
2024-06-12 15:41:24 +0100

    fit model on temp training data

--- 03_modelTraining.R
+++ 03_modelTraining.R
@@ -2,6 +2,7 @@
 library(tidymodels)
 library(themis)
 library(skimr)
+library(tidyverse)
 #library(parsnip) don't thin I actually need this
 library(randomForest)
 
@@ -33,6 +34,7 @@ splitData_fctn <- function(data, proportion){
   return(list("train" = as.data.frame(trainData), "test"=as.data.frame(testData)))
 }
 
+#apply split data function to all matrices
 trainTest <- lapply(X=inputForML, FUN = splitData_fctn, proportion =0.8)
 
 trainData <- lapply(trainTest, function(w){
@@ -58,8 +60,8 @@ processingRec <- lapply(X=inputForML, FUN=preprocessData_fctn)
 
 
 str(inputForML[[2]])
-temp <- inputForML[[2]]
-
+temp <- inputForML[[5]]
+#temp <- temp %>% select(1,4,5,6,7,10)
 
 #convert outcome to a factor
 #temp$significant <- as.factor(temp$significant)
@@ -79,8 +81,8 @@ ggplot(tempTrain, aes(factor(significant)))+
 
 
 temp_rec <- recipe(significant ~ ., data = tempTrain) %>%
-  step_downsample(significant, under_ratio =1) %>%
-  step_select_vip()
+  step_downsample(significant, under_ratio =1)
+  #step_select_vip()
 temp_rec %>% prep() %>% bake(NULL)
 
 
@@ -97,32 +99,83 @@ temp_rec %>%
 
 
 # set seed for CV:
-
-cv_folds <- vfold_cv(
-  data = tempTrain,
-  v = 5
-)
+?yardstick::metric_set
+cv_folds <- vfold_cv(data = tempTrain, v = 5)
 
 #model specification: RF
 
 show_engines("rand_forest")
-rf_mod <- rand_forest(trees = 100, 
-                      mtry = tune(), min_n = tune(),
+rf_mod <- rand_forest(trees = 500, 
+                      mtry = tune(),#min_n = tune(),
                       mode = "classification") %>%
   set_engine("randomForest", importance = TRUE)
 
 rf_tune_grid <- grid_regular(
-  mtry(range = c(1,4)),
-  levels = 4
+  mtry(range = c(1,6)),
+  #min_n(range = c(1,2)),
+  levels = 6
 )
-##workflow:
+##build workflow:
 temp_wf <- workflow() %>%
   add_recipe(temp_rec) %>%
   add_model(rf_mod)
 
+#doParallel::registerDoParallel()
+
+#tune model:
 tune_rf <- tune_grid(
   temp_wf,
   resamples = cv_folds,
-  grid = 4
+  grid = rf_tune_grid,
+  control = control_grid(save_pred = TRUE),
+  metrics = metric_set(roc_auc)
 ) #had to install package randomForest for this to work
 ##now fit model on 
+#can look at the roc  for each combination of parameters
+autoplot(tune_rf)
+#the exact same plot above:
+plot <- tune_rf%>%
+  collect_metrics()%>%
+  ggplot(aes(x=mtry, y=mean))+
+  geom_point()+
+  geom_line()
+plot
+
+
+#best <- tune_rf %>% collect_metrics() %>%
+ # arrange(.metric)
+#collect_metrics(tune_rf)
+#select_best(tune_rf, metric = "roc_auc")
+#############################fit_mod <- best_fit(temp_wf, split = dataSplit, metric_set(roc_auc))
+best_mod <- tune_rf %>% select_best(metric = "roc_auc")
+
+#fit the final model with best params on training
+final_fit <- finalize_workflow(temp_wf, best_mod) %>%
+  fit(data = tempTrain)
+
+
+
+#final_fit%>% extract_fit_parsnip()%>%tidy()
+#library(broom)
+#ff <- finalize_workflow(temp_wf, best_mod)
+ffff <- best_mod %>% last_fit(split = dataSplit)
+
+
+library(vip)
+
+final_fit %>% extract_fit_parsnip() %>%
+  vip(geom = 'col', aesthetics = list(colour="black", fill ="lightblue", alpha=0.7)) +
+  theme_classic()
+
+
+#get the test performance
+temp_aug <- augment(final_fit, tempTest)
+head(temp_aug)
+roc_auc(temp_aug, significant, .pred_TRUE)
+conf_mat(temp_aug, significant, .pred_class)
+
+#plot ROC curve:
+two_classCurve <- roc_curve(temp_aug, truth = significant, .pred_TRUE)
+autoplot(two_classCurve)
+roc_auc(temp_aug, significant, .pred_FALSE)
+
