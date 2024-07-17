d521e8c972f85ae49287d069373cc0bd8b5d66c7
kusqaum <kusqaum1@gmail.com>
2024-07-17 15:08:07 +0100

    integrate gene expression data with network, perform NMF

--- 08_integrateNetwork.R
+++ 08_integrateNetwork.R
@@ -1,4 +1,12 @@
 #integrate network data with gene expression data
+library(tidyverse)
+#might move this to a different script
+library(tidymodels)
+library(themis)
+library(vip)
+library(doParallel)
+library(foreach)
+library(NMF)
 
 
 #embeddings created using pecanpy in python
@@ -6,11 +14,11 @@ embeddings <- read.table("processed/networkEdgeList.emb", sep = "\t", skip = 1)
 colnames(embeddings)
 
 #read in gene expression data:
-fullGeforPCA <- readRDS("processed/fullGeneExpressForPCA.rds")
-head(fullGeforPCA[1:5,1:3])
-#and labels:
-labelsFullDf <- read.table("processed/labelsFullDf.txt", header = T, sep = "\t")
-head(labelsFullDf)
+# fullGeforPCA <- readRDS("processed/fullGeneExpressForPCA.rds")
+# head(fullGeforPCA[1:5,1:3])
+# #and labels:
+# labelsFullDf <- read.table("processed/labelsFullDf.txt", header = T, sep = "\t")
+# head(labelsFullDf)
 #importing node2vec result now
 #split into 1000 dims because that is the dimensions used for pecanPy
 readNetworkData <- function(embData, dimens){
@@ -23,219 +31,90 @@ readNetworkData <- function(embData, dimens){
 }
 
 #just separate because all has been put into one column
-embTest <- embeddings %>%
-  separate(colnames(embeddings), c("hgnc_symbol", paste0("D", 1:500)), sep = " ")
+# embTest <- embeddings %>%
+#   separate(colnames(embeddings), c("hgnc_symbol", paste0("D", 1:500)), sep = " ")
 
 # embTest <- mutate_all(embTest, function(x) as.numeric(as.character(x)))
 # rownames(emb) <- emb$D0
 
 
 emb <- readNetworkData("processed/networkEdgeList.emb", dimens = 500)
-origEmb <-readNetworkData("processed/origNetworkEdgeList.emb", 1000)
-dim(origEmb)
-head(origEmb[1:4,1:6])
-head(origEmb[,999:1001])
+#origEmb <-readNetworkData("processed/origNetworkEdgeList.emb", 1000)
+#dim(origEmb)
+dim(emb)
+#head(origEmb[1:4,1:6])
+#head(origEmb[,999:1001])
 #now need to convert the genes to ensembl IDs 
-netGenes <- data.frame(hgnc_symbol = origEmb$hgnc_symbol)
+netGenes <- data.frame(hgnc_symbol = emb$hgnc_symbol)
 dim(netGenes)
 networkGenes <- data.frame(hgnc_symbol = emb$hgnc_symbol)
 write.table(netGenes, "processed/networkGenesExportForMapping.txt", sep = "\t", row.names = F, quote = F)
-write.table(networkGenes,"processed/AllnetworkGenesExportForMapping.txt", sep = "\t", row.names = F, quote = F)
+#write.table(networkGenes,"processed/AllnetworkGenesExportForMapping.txt", sep = "\t", row.names = F, quote = F)
 
 ##now read in the mapped file 
-# networkGenesWithEnsembl <- read.table("raw/networkGenesWithEnsembl.txt", sep = "\t", header = T)
-networkGenesWithEnsembl <- read.table("raw/networkGenesWithEnsembl.txt", sep = "\t", header = T)
+
+networkGenesWithEnsembl <- read.table("raw/allNetworkGenesMapped.txt", sep = "\t", header = T)
 # networkGenesWithEnsembl <- networkGenesWithEnsembl %>% 
 #   distinct(hgnc_symbol, .keep_all = T)
 #embTest$hgnc_symbol <- embTest$D0
 #rownames(networkGenesWithEnsembl)<- networkGenesWithEnsembl$hgnc_symbol
 # netData_2 <- merge(networkGenesWithEnsembl, embTest, by="hgnc_symbol")
-networkData <- merge(networkGenesWithEnsembl, origEmb, by="hgnc_symbol")
+networkData <- merge(networkGenesWithEnsembl, emb, by="hgnc_symbol")
 head(networkData[1:4,1:3])
 rownames(networkData)<- networkData$ensembl_gene_id; networkData$hgnc_symbol<- NULL; networkData$ensembl_gene_id<-NULL
 str(networkData$D2)
 dim(networkData)
-head(networkData[,996:1000])
+head(networkData[,488:500])
 
 #now need to merge with gene exp
 networkData <- mutate_all(networkData, function(x) as.numeric(as.character(x)))
 
-fullGPCA <- merge(fullGeforPCA, networkData, by = 0)
-head(fullGPCA[1:3,1:3])
-dim(fullGPCA)
-head(fullGPCA[15342:15346,3310:3312])
-rownames(fullGPCA)<- fullGPCA$Row.names; fullGPCA$Row.names <- NULL
-plan(multisession, workers=availableCores())
-dim(fullGPCA)
-head(fullGPCA[15339:15346, 3309:3312])
-integratedDataPcaRes <- prcomp(fullGPCA, scale=T)
-integPCs <- as.data.frame(integratedDataPcaRes$x)
-#newpcares <- prcomp(fullGPCA, scale. = T)
+sVGeneExpress <- readRDS("processed/fullGeneExpressForNMF.rds")
+geneExpressWithNet <- merge(sVGeneExpress, networkData, by = 0)
+head(geneExpressWithNet[1:3,1:3])
 
-integ_200dim <- as.data.frame(integPCs)[,1:200]
-integ_50dim <- as.data.frame(integPCs)[,1:50]
+dim(geneExpressWithNet)
 
-integlabelled_50dim <- merge(integ_50dim, labelsFullDf, by=0); integlabelled_50dim$Row.names<-NULL
-integlabelled_200dim <- merge(integ_200dim, labelsFullDf, by=0); integ_100dim$Row.names<-NULL
+rownames(geneExpressWithNet)<- geneExpressWithNet$Row.names; geneExpressWithNet$Row.names <- NULL
+minVal <- abs(min(geneExpressWithNet))
+minVal
+geneExpressWithNetoffSet <- geneExpressWithNet+minVal
+ranks = c(5,10,50,100,150,200,500)
+noofruns <- 2
+set.seed(1234)
 
-#split data and create rec
-integsplit200 <- split_processingData_fctn(integlabelled_200dim, 0.8)
-integsplit50<- split_processingData_fctn(integlabelled_50dim, 0.8)
-
-#ml
-plan(multisession, workers=availableCores())
-plan(multisession, workers=availableCores())
-plan(multisession, workers = availableCores())
-mlresultWithPPI_200dim <- justToTestRF(integsplit200, algorithm = "PCA")
-mlresultWithPPI_50dim <- justToTestRF(integsplit50, algorithm = "PCA")
-
-##
-
-
-
-
-#here are the functions used:
-split_processingData_fctn <- function(data, proportion){
-  set.seed(123)
-  dataSplit <- initial_split(data, prop = proportion)
-  trainData <- training(dataSplit)
-  testData <- testing(dataSplit)
-  #create recipe
-  rec <-recipe(significant~., data = trainData) %>%
-    step_downsample(significant, under_ratio = 1, seed = 456)
-  #return(list(rec, trainData, testData))
-  return(list("train" = as.data.frame(trainData), "test"=as.data.frame(testData), "recipe"=rec))
-}
-
-
-justToTestRF <- function(preprocessResult, algorithm){
-  if(algorithm == "NMF"){
-    model_RF <- rand_forest(trees = 500, mtry = sqrt(ncol(preprocessResult$train)), min_n = tune(), 
-                            mode = "classification") %>% set_engine("randomForest", importance = TRUE)
-    set.seed(234)
-    
-    folds <- vfold_cv(data = preprocessResult$train, v=3)
-    limit <- (ncol(preprocessResult$train))-1
-    tuningGrid <- grid_regular(
-      #trees(range = c(1,2000)),
-      #mtry(range = c(1,limit)),
-      min_n(range = c(1,limit)),
-      levels = limit
-    )
-    #build workflow
-    wkflow <- workflow() %>%
-      add_recipe(preprocessResult$recipe) %>%
-      add_model(model_RF)
-    #tune model
-    #plan(multisession, workers = 16)
-    res <- tune_grid(
-      wkflow,
-      resamples = folds,
-      grid = tuningGrid,
-      control = control_grid(save_pred = TRUE),
-      metrics = metric_set(roc_auc),
-    )
-    resDf <- res %>% collect_metrics() %>%
-      mutate(dim = ncol(preprocessResult$train)-1, Algorithm = algorithm)
-    paramsPlot <- autoplot(res)
-    paramsPlot2 <- res %>%
-      collect_metrics() %>%
-      ggplot(aes(x=mtry, y=mean, col=as.factor(min_n)))+
-      geom_point()+
-      geom_line()
-    
-    final_model <- res %>% select_best(metric = "roc_auc")
-    
-    final_fit <- finalize_workflow(wkflow, final_model) %>%
-      parsnip::fit(data = preprocessResult$train) 
-    
-    importancePlot <- final_fit %>% extract_fit_parsnip()%>%
-      vip(geom='point',aes = list(colour="black", fill='lightblue', alpha=0.7))+
-      theme_classic()
-    
-    importanceDf <- final_fit %>% extract_fit_parsnip() %>%
-      vi() %>% as.data.frame()
-    
-    aug <- augment(final_fit, preprocessResult$test)
-    aug_m <- aug %>% mutate(dim = ncol(preprocessResult$train)-1, Algorithm = algorithm)
-    
-    roc_auc <- roc_auc(aug, significant, .pred_TRUE)
-    two_classCurve <- roc_curve(aug, truth = significant,
-                                .pred_TRUE)
-    rocCurve <- autoplot(two_classCurve)
-    
-    #dims <- c("5pca","10pca","50pca","100pca","150pca","200pca")
-    result <- list("workflow" = wkflow, "res" = res, "resDf" = resDf, "finalMod" = final_model, "tuningPlots" =paramsPlot, "importancePlot"=importancePlot, "importanceDf"=importanceDf,
-                   "finalFit" = final_fit, "AUC"= roc_auc, "roc_curve" = rocCurve, "aug"= aug_m)
-    # saveRDS(result, sprintf("%sGTEXtestingMLRes_%s.rds",algorithm, (ncol(preprocessResult$train)-1)))
-    
-    return(list("workflow" = wkflow, "res" = res, "resDf" = resDf, "finalMod" = final_model, "tuningPlots" =paramsPlot, "importancePlot"=importancePlot, "importanceDf"=importanceDf,
-                "finalFit" = final_fit, "AUC"= roc_auc, "roc_curve" = rocCurve, "aug"= aug_m))
-    
-  }
-  else if(algorithm == "PCA"){
-    model_RF <- rand_forest(trees = 500, mtry = sqrt(ncol(preprocessResult$train)), min_n = tune(), 
-                            mode = "classification") %>% set_engine("randomForest", importance = TRUE)
-    set.seed(234)
-    
-    folds <- vfold_cv(data = preprocessResult$train, v=3)
-    limit <- (ncol(preprocessResult$train))-1
-    tuningGrid <- grid_regular(
-      #trees(range = c(1,2000)),
-      #mtry(range = c(1,limit)),
-      min_n(range = c(1,limit)),
-      levels = limit
-    )
-    #build workflow
-    wkflow <- workflows::workflow() %>%
-      add_recipe(preprocessResult$recipe) %>%
-      add_model(model_RF)
-    #tune model
-    #plan(multisession, workers = 16)
-    res <- tune_grid(
-      wkflow,
-      resamples = folds,
-      grid = tuningGrid,
-      control = control_grid(save_pred = TRUE),
-      metrics = metric_set(roc_auc),
-    )
-    resDf <- res %>% collect_metrics() %>%
-      mutate(dim = ncol(preprocessResult$train)-1, Algorithm = algorithm)
-    paramsPlot <- autoplot(res)
-    paramsPlot2 <- res %>%
-      collect_metrics() %>%
-      ggplot(aes(x=mtry, y=mean, col=as.factor(min_n)))+
-      geom_point()+
-      geom_line()
-    
-    final_model <- res %>% select_best(metric = "roc_auc")
-    
-    final_fit <- finalize_workflow(wkflow, final_model) %>%
-      parsnip::fit(data = preprocessResult$train) 
-    
-    importancePlot <- final_fit %>% extract_fit_parsnip()%>%
-      vip(geom='point',aes = list(colour="black", fill='lightblue', alpha=0.7))+
-      theme_classic()
-    
-    importanceDf <- final_fit %>% extract_fit_parsnip() %>%
-      vi() %>% as.data.frame()
-    
-    aug <- augment(final_fit, preprocessResult$test)
-    aug_m <- aug %>% mutate(dim = ncol(preprocessResult$train)-1, Algorithm = algorithm)
-    
-    roc_auc <- roc_auc(aug, significant, .pred_TRUE)
-    two_classCurve <- roc_curve(aug, truth = significant,
-                                .pred_TRUE)
-    rocCurve <- autoplot(two_classCurve)
-    
-    #dims <- c("5pca","10pca","50pca","100pca","150pca","200pca")
-    result <- list("workflow" = wkflow, "res" = res, "finalMod" = final_model, "tuningPlots" =paramsPlot, "importancePlot"=importancePlot, "importanceDf"=importanceDf,
-                   "finalFit" = final_fit, "AUC"= roc_auc, "roc_curve" = rocCurve, "aug"= aug_m)
-    #saveRDS(result, sprintf("processed/%sGTEXtestingMLRes_%s.rds",algorithm, ncol(preprocessResult$train)-1))
-    
-    return(list("workflow" = wkflow, "res" = res, "resDF"=resDf, "finalMod" = final_model, "tuningPlots" =paramsPlot, "importancePlot"=importancePlot, "importanceDf"=importanceDf,
-                "finalFit" = final_fit, "AUC"= roc_auc, "roc_curve" = rocCurve, "aug"= aug_m))
-  }
-  
-}
+cl <- makeCluster(25, outfile="processed/nmfSvWithNetwork.txt")
+registerDoParallel(cl)
+# integratedDataPcaRes <- prcomp(fullGPCA, scale=T)
+print("running NMF")
+resInteg.multiRank <- nmf(geneExpressWithNetoffSet, rank = ranks, nrun=noofruns, seed = 123456)
 #
+
+saveRDS(resInteg.multiRank,"processed/resInteg.multiRank.rds")
+
+
+# integPCs <- as.data.frame(integratedDataPcaRes$x)
+newpcares <- prcomp(networkData, scale. = T)
+integPCs <- newpcares$x
+head(integPCs[1:5,1:4])
+integ_100dim <- as.data.frame(integPCs)[,1:10]
+# integ_50dim <- as.data.frame(integPCs)[,1:50]
+
+# integlabelled_50dim <- merge(integ_50dim, labelsFullDf, by=0); integlabelled_50dim$Row.names<-NULL
+integlabelled_100dim <- merge(integ_100dim, labelsFullDf, by=0); integ_100dim$Row.names<-NULL
+rownames(integlabelled_100dim) <- integlabelled_100dim$Row.names; integlabelled_100dim$Row.names <-NULL
+# #split data and create rec
+integsplit100 <- split_processingData_fctn(integlabelled_100dim, 0.8)
+# integsplit50<- split_processingData_fctn(integlabelled_50dim, 0.8)
+# 
+# #ml
+# 
+plan(multisession, workers=20)
+mlresultWithPPI_100dim <- justToTestRF(integsplit100, algorithm = "PCA")
+# mlresultWithPPI_50dim <- justToTestRF(integsplit50, algorithm = "PCA")
+# 
+# ##
+
+
+
+
