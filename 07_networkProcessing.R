4a5dd9012b43cf6c69969ed46a2eb53e52e23b95
kusqaum <kusqaum1@gmail.com>
2024-08-31 20:16:24 +0100

    final commit

--- 07_networkProcessing.R
+++ 07_networkProcessing.R
@@ -1,14 +1,19 @@
 ##
 library(igraph)
 library(stringr)
+# first need biogrid data downloaded if i want to run this script!
+# source("code/07a_getBiogridnetwork.R")
+
 # biogridNetwork <- read.table("raw/bioGridNetworkData.txt", header = T)
 biogridNetwork <- read.table("raw/allNetworkData.txt", header = T)
+# make undirected
 graph <- graph_from_data_frame(biogridNetwork, directed = F)
-vcount(graph)
 
 
+# simplify() to remove duplicate edges
 graphS <- simplify(graph)
 
+# get the largest connected component
 components <- components(graphS)
 groups(components)
 # https://stackoverflow.com/questions/64344845/getting-the-biggest-connected-component-in-r-igraph
@@ -20,9 +25,14 @@ class(vertID)
 subgraph <- igraph::induced_subgraph(graphS, vertID)
 subgraph
 
-edgeList <- as_edgelist(subgraph, names = T)
+# edgeList <- as_edgelist(subgraph, names = T)
+edgeList <- as.data.frame(as_edgelist(subgraph, names = T))
+dim(edgeList)
 write.table(edgeList, "processed/networkEdgeList.edg", row.names = F, col.names = F, sep = "\t")
+dim(edgeList)
+head(edgeList)
+
+# then use PecanPy in python to run node2vec on edge list
 
 
-# temp <- read.table("processed/networkEdgeList.edg", col.names = F)
 
