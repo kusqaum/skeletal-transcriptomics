b9f295eda5d51d51e3120a485a6c7de4f04285a0
kusqaum <kusqaum1@gmail.com>
2024-08-31 17:24:11 +0100

    read in mortality/aging genes from IMPC

--- phenotype_labels.R
+++ phenotype_labels.R
@@ -22,3 +22,28 @@ unique_false <- small_false %>% distinct(marker_symbol, .keep_all = T)
 
 #combine
 allgenes <- bind_rows(unique_true, unique_false)
+allgenes <- as.logical(allgenes$significant)
+write.table(allgenes, "processed/IMPC_phenotypeAssociations.txt", sep = "\t", quote = F, row.names = F)
+
+
+
+####
+# extract all IMPC genes associated with mortality/aging:
+mortality_aging <- filt %>% filter(grepl("MP:0010768", top_level_mp_term_id))
+mortalityT <- mortality_aging %>% filter(significant == "true") %>% 
+  distinct(marker_symbol, .keep_all = T) %>% select(marker_symbol, significant, marker_accession_id)
+mortalityF <- mortality_aging %>% filter(significant=="false") %>% distinct(marker_symbol, .keep_all = T) %>%
+  select(marker_symbol, significant , marker_accession_id)
+allMortality <- rbind(mortalityT, mortalityF)
+
+allMortality$significant <- as.logical(allMortality$significant)
+allMortalityPhen <- allMortality[order(allMortality[,"marker_symbol"], -allMortality[,'significant']),]
+allMortAging <- allMortalityPhen[!duplicated(allMortalityPhen$marker_symbol),]
+length(which(allMortAging$significant=="TRUE"))
+length(which(allMortAging$significant=="FALSE"))
+head(allMortAging)
+dim(allMortAging)
+length(which(allMortAging$significant=="TRUE"))
+length(which(allMortAging$significant=="FALSE"))
+write.table(allMortAging, "processed/mortalityPhenotype.txt", sep = "\t", quote = F, row.names = F)
+
